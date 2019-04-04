9e93a4d26569ddb774e4889b221033df71481af2
Calypso Day <calypso.day@yahoo.com>
2019-04-04 14:40:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-function shout(string){
-  return string.toUpperCase();
+function shout(sentence){
+  return sentence.toUpperCase();
 }
-function whisper(string){
-  return string.toLowerCase();
+
+function whisper(sentence){
+  return sentence.toLowerCase();
 }
+
 function logShout(sentence){
   console.log(sentence.toUpperCase());
 }
+
 function logWhisper(sentence){
   console.log(sentence.toLowerCase());
 }
+
 function sayHiToGrandma(sentence){
   if (sentence === sentence.toUpperCase()){
     return "YES INDEED";
   } else {
-    return "I can\'t hear you";
+    return "I can\'t hear you!";
   }
 }
